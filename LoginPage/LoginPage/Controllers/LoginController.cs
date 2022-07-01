6a35229e920c0d992dff0019c7e761e902cee525
Niharika4 <niharikaraj1818@gmail.com>
2022-07-01 09:19:54 +0530

    first

---
+++ LoginPage/LoginPage/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(LoginPage.Models.tblUser userModel)
+        {
+            using (LoginDataBaseEntities db = new LoginDataBaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong Usename or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index","Home");
+                }
+            }
+        }
+    }
+}
